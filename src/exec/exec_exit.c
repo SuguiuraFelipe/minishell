450143758f4e8a736f94e5ceae2d9c37da8eb0c5
SuguiuraFelipe <felipesuguiura.pro@gmail.com>
2025-11-05 19:42:41 +0000

    (fix) changing the exec_exit functionto free envp duplicated

--- src/exec/exec_exit.c
+++ src/exec/exec_exit.c
@@ -3,19 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   exec_exit.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: devjorginho <devjorginho@student.42.fr>    +#+  +:+       +#+        */
+/*   By: fsuguiur <fsuguiur@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 15:02:09 by devjorginho       #+#    #+#             */
-/*   Updated: 2025/11/04 09:20:39 by devjorginho      ###   ########.fr       */
+/*   Updated: 2025/11/05 19:40:32 by fsuguiur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-void	exec_exit(char **args, char **dup_envp)
+void exec_exit(char **args, char **envp)
 {
-	(void)args;
-	(void)dup_envp;
-	printf("exit\n");
-	exit(0);
+    (void)args;
+    free_split(envp);
+    if (isatty(STDIN_FILENO))
+        write(1, "exit\n", 5);
+    exit(g_status);
 }
