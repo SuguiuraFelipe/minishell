dbfc811fb594c9821ad5655406744b005467de3d
SuguiuraFelipe <felipesuguiura.pro@gmail.com>
2025-11-06 17:19:38 +0000

    (fix) remove quotes from export

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,13 +1,13 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <stdio.h>
 # include <errno.h>
 # include <fcntl.h>
 # include <readline/history.h>
 # include <readline/readline.h>
 # include <signal.h>
 # include <stddef.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <sys/stat.h>
--- src/exec/exec_export.c
+++ src/exec/exec_export.c
@@ -3,10 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   exec_export.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
+/*   By: fsuguiur <fsuguiur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 15:03:11 by devjorginho       #+#    #+#             */
+/*   Updated: 2025/11/06 17:14:58 by fsuguiur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/minishell.h"
+
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_export.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
 /*   By: jde-carv <jde-carv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/22 15:03:11 by devjorginho       #+#    #+#             */
-/*   Updated: 2025/11/06 16:03:01 by jde-carv         ###   ########.fr       */
+/*   Updated: 2025/11/06 19:18:42 by jde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,30 +73,41 @@ static int	dont_have_value(char *var_value, int i)
 	return (0);
 }
 
+/*
+** Exporta variáveis de ambiente no formato NAME=VALUE
+** - Remove aspas do VALUE ("ls -l" → ls -l)
+** - Substitui variáveis já existentes
+*/
 void	exec_export(char **args, char **dup_envp)
 {
 	int		i;
 	int		j;
-	char	*new_var;
+	char	*var_name;
 	char	*var_value;
+	char	*clean_value;
+	char	*new_entry;
 
 	j = 0;
 	i = 1;
 	while (args[i])
 	{
-		new_var = args[i];
-		var_value = ft_strchr(new_var, '=');
+		var_value = ft_strchr(args[i], '=');
 		if (dont_have_value(var_value, i))
 			break ;
 		*var_value = '\0';
+		var_name = args[i];
 		var_value++;
-		if (!var_already_exist(dup_envp, var_value, new_var))
+		clean_value = remove_quotes(var_value);
+		new_entry = var_concatenation(var_name, "=", clean_value);
+		if (!var_already_exist(dup_envp, var_name, new_entry))
 		{
 			while (dup_envp[j])
 				j++;
-			dup_envp[j] = ft_strdup(var_concatenation(new_var, "=", var_value));
+			dup_envp[j] = ft_strdup(new_entry);
 			dup_envp[j + 1] = NULL;
 		}
+		free(clean_value);
+		free(new_entry);
 		i++;
 	}
 }
